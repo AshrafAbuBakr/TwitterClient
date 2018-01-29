40dadb7eda1cd09ed9e2b2780e3fb6646907f268
Ashraf Abu Bakr <ashraf.abubakr1@gmail.com>
2018-01-30 00:53:33 +0200

    - Added clickable images in follower details.

--- TwitterClient/Features/FollowerDetails/StickyHeaderView.swift
+++ TwitterClient/Features/FollowerDetails/StickyHeaderView.swift
@@ -17,6 +17,9 @@ class StickyHeaderView: UIView {
 	@IBOutlet weak var handleLabel: UILabel!
 	@IBOutlet weak var backgroundImageViewBottomConstraint: NSLayoutConstraint!
 	
+	var backgroundTapGestureRecognizer: UITapGestureRecognizer?
+	var followerImageTapGestureRecognizer: UITapGestureRecognizer?
+	
 	// MARK: - init
 	override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -35,6 +38,10 @@ class StickyHeaderView: UIView {
 	
 	
 	func setupView(withFollower follower: FollowersModel) {
+		
+		backgroundTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundImageTapped))
+		followerImageTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(followerImageTapped))
+		
 		var defaultColorImage = UIImage(color: UIColor.color(string: "1dcaff"))
 		if let bgCode = follower.profile_background_color {
 			defaultColorImage = UIImage(color: UIColor.color(string: bgCode))
@@ -45,6 +52,7 @@ class StickyHeaderView: UIView {
 			if backgroundImageURL.length > 0 {
 				let resource = ImageResource(downloadURL: URL(string: backgroundImageURL)!)
 				backgroundImageView.kf.setImage(with: resource)
+				backgroundImageView.addGestureRecognizer(backgroundTapGestureRecognizer!)
 			}
 		}
 		
@@ -52,6 +60,7 @@ class StickyHeaderView: UIView {
 			if followerImageURL.length > 0 {
 				let followerResource = ImageResource(downloadURL: URL(string: followerImageURL)!)
 				followerImageView.kf.setImage(with: followerResource)
+				followerImageView.addGestureRecognizer(followerImageTapGestureRecognizer!)
 			}
 		}
 		
@@ -59,6 +68,60 @@ class StickyHeaderView: UIView {
 		if follower.screen_name .length > 0 {
 			handleLabel.text = "@\(follower.screen_name)"
 		}
+	}
+	
+	@objc private func backgroundImageTapped() {
+		let canceltapGesture = UITapGestureRecognizer(target: self, action: #selector(removeImage))
+		let containerView = UIView(frame: UIScreen.main.bounds, backgroundColor: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6))
+		containerView.tag = -999
+		containerView.alpha = 0.0
+		containerView.addGestureRecognizer(canceltapGesture)
+		UIApplication.shared.keyWindow?.addSubview(containerView)
+		let imageView = UIImageView(frame: backgroundImageView.bounds)
+		imageView.image = backgroundImageView.image
+		self.addSubview(imageView)
+		
+		UIView.animate(withDuration: 0.3, animations: {
+			imageView.center = containerView.convert(containerView.center, to: self)
+		}) { (completed) in
+			containerView.alpha = 1.0
+			containerView.addSubview(imageView)
+			imageView.center = containerView.center
+		}
+	}
+	
+	@objc private func followerImageTapped() {
+		let canceltapGesture = UITapGestureRecognizer(target: self, action: #selector(removeImage))
+		let containerView = UIView(frame: UIScreen.main.bounds, backgroundColor: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6))
+		containerView.tag = -999
+		containerView.alpha = 0.0
+		containerView.addGestureRecognizer(canceltapGesture)
+		UIApplication.shared.keyWindow?.addSubview(containerView)
+		let imageView = UIImageView(frame: followerImageView.frame)
+		imageView.image = followerImageView.image
+		self.addSubview(imageView)
+		
+		UIView.animate(withDuration: 0.3, animations: {
+			imageView.transform = imageView.transform.scaledBy(x: 2, y: 2)
+			imageView.center = containerView.convert(containerView.center, to: self)
+		}) { (completed) in
+			containerView.alpha = 1.0
+			containerView.addSubview(imageView)
+			imageView.center = containerView.center
+		}
+		
+	}
+	
+	@objc private func removeImage() {
+		if let container = UIApplication.shared.keyWindow?.viewWithTag(-999) {
+			UIView.animate(withDuration: 0.3, animations: {
+				container.alpha = 0.0
+			}, completion: { (completed) in
+				container.removeFromSuperview()
+			})
+			
+		}
+		
 	}
 	
 	func updateView(withScrollviewOffset offsetY: CGFloat) {
